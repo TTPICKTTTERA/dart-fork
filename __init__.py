b3622697f4dd10b6317c824a6e0520f4648d5d0c
Ethan M. Kim <39701330+ekimoco@users.noreply.github.com>
2026-04-06 12:59:56 +0900

    Re-export with name `dart` instead of `OpenDartReader` as it shadows the class `dart.OpenDartReader`

--- __init__.py
+++ __init__.py
@@ -6,4 +6,4 @@ from .dart import *
 __version__ = '0.2.2'
 __all__ = ['__version__', 'OpenDartReader']
 
-sys.modules['OpenDartReader'] = dart.OpenDartReader
+sys.modules['dart'] = dart.OpenDartReader
